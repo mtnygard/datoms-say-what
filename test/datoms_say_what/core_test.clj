08426dd7bd2b209707b08bc4181cae197f93df36
Michael Nygard <mtnygard@cognitect.com>
2017-08-16 11:22:49 -0400

    Empty project structure

---
+++ test/datoms_say_what/core_test.clj
@@ -0,0 +1,7 @@
+(ns datoms-say-what.core-test
+  (:require [clojure.test :refer :all]
+            [datoms-say-what.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
