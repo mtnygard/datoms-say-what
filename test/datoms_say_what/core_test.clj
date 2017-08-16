25b157141637e75887452b101899f24775c57f2f
Michael Nygard <mtnygard@cognitect.com>
2017-08-16 15:50:29 -0400

    Remove boilerplate test

--- test/datoms_say_what/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns datoms-say-what.core-test
-  (:require [clojure.test :refer :all]
-            [datoms-say-what.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
