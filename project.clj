5e9759689e5dcc63cb9a1f22cafc1834d9a7bd57
Michael T. Nygard <mtnygard@gmail.com>
2017-08-25 11:45:58 -0500

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mtnygard/datoms-say-what "1.0.0-SNAPSHOT"
+(defproject mtnygard/datoms-say-what "1.0.0"
   :description   "Visualize the results of a Datomic transaction"
   :url           "http://github.com/mtnygard/datoms-say-what"
   :license       {:name "Eclipse Public License"
